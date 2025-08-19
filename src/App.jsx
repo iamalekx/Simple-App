4bbc601ddc1b4ebcaa02c88c0cbc91a6ea53e5bd
iamalekx <lexaamrh@gmail.com>
2025-08-19 14:28:14 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,22 @@ import "./App.css";
 import Card from "./components/Card";
 import Button from "./components/Button";
 import "./components/styles.css"
+import image1 from "./images/dog-funny-glasses-wooden-background-cute-5k-6000x4000-1810.jpg";
+import image2 from "./images/coffee-cup-instant-coffee-blue-background-ceramic-mug-top-5616x3744-3327.jpg";
+import image3 from "./images/minion-halloween-7680x4320-18810.jpg";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <Card />
-            <Button />
+            <Card title="Dog" price="USD 150.00" image={image1}>
+            </Card>
+            <Card title="Coffee" price="USD 10.00" image={image2}>
+            </Card>
+            <Card title="Minions" price="USD 50.00" image={image3}>
+            </Card>
+            {/* <Button /> */}
         </>
     );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,15 @@
-function Card() {
-    return <h1>Name: Lexis Amarh</h1>;
+function Card({ title, price, image }) {
+    return (
+        <div className="card">
+            <div className="image">
+                <img src={image} alt="..." />
+            </div>
+            <div className="content">
+                <h2>{title}</h2>
+                <p>{price}</p>
+            </div>
+        </div>
+    );
 }
 
 export default Card;
